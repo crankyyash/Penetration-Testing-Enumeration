506b492f4b878c6ee2d2ec2052847bafbeda122b
crankyyash <61792333+crankyyash@users.noreply.github.com>
2024-01-30 23:04:29 +0530

    Add files via upload
    Insert path to subnets.txt into batch script which contains list of subnets per line to pingsweep
    All ips which respond to pingsweep will be outputted to ping-UP.txt and the rest of the unreachable ips will be outputted in ping-DOWN.txt

---
+++ pingsweep-cmd-multiple-subnets.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /f "delims=" %%i in (subnets.txt) do (
+    echo Working on %%i subnet
+    set "ipAddress=%%i"
+    for /f "tokens=1-4 delims=." %%a in ("!ipAddress!") do (
+        set "octet1=%%a"
+        set "octet2=%%b"
+        set "octet3=%%c"
+        set "octet4=%%d"
+    )
+    for /L %%k in (1,1,254) do (
+        echo. & echo Trying !octet1!.!octet2!.!octet3!.%%k
+        ping -n 1 -w 1000 !octet1!.!octet2!.!octet3!.%%k >nul  
+        if !errorlevel! == 0 (
+REM            echo !octet1!.!octet2!.!octet3!.%%k UP >> C:\Users\Admin\Documents\exceptions-tools-scripts\ping-UP.txt
+			echo !octet1!.!octet2!.!octet3!.%%k >> C:\Users\Admin\Documents\exceptions-tools-scripts\ping-UP.txt
+        ) else (
+            echo !octet1!.!octet2!.!octet3!.%%k DOWN >> C:\Users\Admin\Documents\exceptions-tools-scripts\ping-DOWN.txt
+        )
+    )
+)
+
+endlocal
