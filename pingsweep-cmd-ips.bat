988ddcf2991fd4ee0dd1e728f22ed5e524b97e55
crankyyash <61792333+crankyyash@users.noreply.github.com>
2024-01-30 23:29:23 +0530

    Update pingsweep-cmd-ips.bat

--- pingsweep-cmd-ips.bat
+++ pingsweep-cmd-ips.bat
@@ -5,7 +5,7 @@ for /f "delims=" %%a in (ips.txt) do (
     echo. & ECHO Trying %%a
     ping -n 1 -w 1000 %%a >nul  
     if !errorlevel! == 0 (
-REM        echo %%a UP >> C:\Users\Admin\Documents\exceptions-tools-scripts\up-ips.txt
+REM        echo %%a UP >> up-ips.txt
         echo %%a >> up-ips.txt
     ) else (
         echo %%a DOWN >> down-ips.txt
